f499654ada3df07410ef9c9c182bc66ace1de23a
Raphael Capeto <raphaelcapeto@gmail.com>
2020-05-06 22:13:03 -0300

    criar a snake

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 let canvas = document.querySelector('canvas');
 let context = canvas.getContext('2d');
 let box = 32;
+let snake = [];
+snake[0] = {
+   x: 8 * box,
+   y: 8 * box
+}
 
 function createBackGround(){
    context.fillStyle = 'lightgreen';
    context.fillRect(0, 0 , 16 * box, 16 * box);
 }
 
+function createSnake(){
+   for(let i = 0; i < snake.length; i++){
+      context.fillStyle = "#000";
+      context.fillRect(snake[i].x, snake[i].y, box, box);
+   }
+}
+
 createBackGround();
+createSnake();
