49f82c6fcc4343fd00731e0c00a9aa56199587dc
Raphael Capeto <raphaelcapeto@gmail.com>
2020-05-06 22:23:34 -0300

    definir movimentos

--- script.js
+++ script.js
@@ -6,18 +6,40 @@ snake[0] = {
    x: 8 * box,
    y: 8 * box
 }
+let direction = 'right';
 
-function createBackGround(){
+function createBackGround() {
    context.fillStyle = 'lightgreen';
    context.fillRect(0, 0 , 16 * box, 16 * box);
 }
 
-function createSnake(){
+function createSnake() {
    for(let i = 0; i < snake.length; i++){
       context.fillStyle = "#000";
       context.fillRect(snake[i].x, snake[i].y, box, box);
    }
 }
 
-createBackGround();
-createSnake();
+function startGame() {  
+   createBackGround();
+   createSnake();
+
+   let snakeX = snake[0].x;
+   let snakeY = snake[0].y;
+   
+   if(direction == 'right') snakeX += box;
+   if(direction == 'left') snakeX -= box;
+   if(direction == 'up') snakeY -= box;
+   if(direction == 'down') snakeY += box;
+
+   snake.pop();
+
+   let newHead = {
+      x: snakeX,
+      y: snakeY
+   };
+
+   snake.unshift(newHead);
+}
+
+let game = setInterval(startGame, 100);
